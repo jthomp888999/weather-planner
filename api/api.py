fbfef5342aecf62cb20b005a1a4a7caeadc787f6
John Thompson <jthomp@protonmail.com>
2020-09-03 13:41:23 -0500

    intial setup with simple flask api

---
+++ api/api.py
@@ -0,0 +1,22 @@
+import os
+
+from flask import Flask
+from requests import get
+
+app = Flask(__name__)
+
+KEY = os.environ["API_KEY"]
+LAT = 30.45
+LON = -91.8
+EXCLUDE = "minutely,hourly,daily"
+UNITS = "imperial"
+URL = "https://api.openweathermap.org/data/2.5/onecall?"\
+    f"lat={LAT}&lon={LON}"\
+    f"&exclude={EXCLUDE}"\
+    f"&units={UNITS}"\
+    f"&appid={KEY}"
+
+@app.route("/current")
+def get_current():
+    r = get(URL)
+    return r.json()

44371205fb4907a60cc5bb4b66584ab3c41fbca3
John Thompson <jthomp@protonmail.com>
2020-09-04 03:55:18 -0500

    same

--- api/api.py
+++ api/api.py
@@ -1,7 +1,9 @@
 from flask import Flask
 
 from api.blueprints import current
+from api.blueprints import next_seven
 
 app = Flask(__name__)
 
 app.register_blueprint(current)
+app.register_blueprint(next_seven)

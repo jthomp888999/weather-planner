a2b4a785a908b501dbf9fa010dcf27adf105c645
John Thompson <jthomp@protonmail.com>
2020-09-04 04:00:20 -0500

    getting raw data to react

--- src/components/nextSeven/nextSeven.jsx
+++ src/components/nextSeven/nextSeven.jsx
@@ -3,7 +3,7 @@ import React, {useEffect, useState} from 'react';
 const NextSeven = () => {
   // useState hold json object with keys
   const [weatherData, setWeatherData] = useState({
-    "humidity": ""
+    "daily": ""
   });
 
   // useEffect to hit api and populate state
@@ -14,7 +14,7 @@ const NextSeven = () => {
     })
     .then(data => {
       setWeatherData({
-        "humidity": data.daily[0].humidity
+        "daily": data.daily
     });
     });
   }, []);

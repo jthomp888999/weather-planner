5786dcfcfc01e43468f3f3cfe0f5988492896739
John Thompson <jthomp@protonmail.com>
2020-09-06 15:43:24 -0500

    next seven days serverside setup

--- api/blueprints/next_seven/next_seven.py
+++ api/blueprints/next_seven/next_seven.py
@@ -1,7 +1,8 @@
 from os import environ
 import json
+from datetime import datetime, timedelta
 
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, jsonify
 from requests import get
 
 from api.common import url_gen
@@ -14,8 +15,21 @@ next_seven = Blueprint('next_seven', __name__)
 @next_seven.route("/api/nextseven", methods=["GET"])
 def nextseven():
     r = get(url_gen(EXCLUDE))
-    d = r.json()
+    week = r.json()["daily"]
 
-    # data = {"test": "it works i guess"}
+    data = []
+    day = {} 
 
-    return d
+    for i in range(len(week)):
+        day["date"] = datetime.fromtimestamp(week[i]['dt'])
+        day["clouds"] = week[i]["clouds"]
+        day["percip"] = week[i]["pop"]
+        day['feels_like'] = week[i]['feels_like']
+        day["humidity"] = week[i]["humidity"]
+        day["wind_speed"] = week[i]["wind_speed"]
+
+        day_copy = day.copy()
+        data.append(day_copy)
+            
+    return jsonify(data)
+    # return jsonify(week)
